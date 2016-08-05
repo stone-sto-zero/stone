addda3a47e721cc67231c92008043115c175c783
wuguoxin <wuguoxin@chunyu.me>
2016-08-05 19:24:16 +0800

    增加s01判定的实现

--- algorithm/momentium.py
+++ algorithm/momentium.py
@@ -29,14 +29,112 @@ def find_peak_period(s01, base_down=0.1, shut_speed=0.001, min_down=0.03, max_up
     :type base_down: float
     :param s01: 数据源
     :type s01: pd.Series
-    :return: 下跌的起始和结束的点, 起始的理想情况是, 有一定的延迟, 结束的理想情况是, 有些许的超前
-    :rtype:list[tuple[str]]
+    :return: 下跌的起始和结束的点, 起始的理想情况是, 有一定的延迟, 结束的理想情况是, 有些许的超前, 0 bottom, 1 top, [(top, date_str)]
+    :rtype:list[tuple]
     """
+    s01 = s01.dropna()
     date_strs = s01.index.values
+    # 当前处于上升还是下降, 0 上升, 1 下降
+    cur_state = 0
+    cur_max_price = s01[0]
+    cur_min_price = s01[0]
+    cur_base_down = base_down
+    cur_up_percent = 0
+    cur_up_days = 0
+    res_dates = list()
+    res_dates.append((0, date_strs[0]))
     for date_str in date_strs:
-        print date_str
+        cur_price = s01[date_str]
+
+        # 处于上升, 做下跌开始的判定
+        if cur_state == 0:
+
+            # 虽然在上升, 但是如果价格低于了最低价, 那也更新下最低价
+            if cur_price < cur_min_price:
+                cur_min_price = cur_price
+
+            # 当前价格高于最高价, 上升继续,
+            if cur_price >= cur_max_price:
+                cur_up_days += 1
+                cur_up_percent = cur_price / cur_min_price - 1
+
+                # 更新base_down
+                if shut_by == 0:
+                    # 按天更新, 直接减就行
+                    if cur_base_down >= min_down + shut_speed:
+                        cur_base_down -= shut_speed
+                elif shut_by == 1:
+                    # 按百分比更新, 算出百分比, 然后更新
+                    points = cur_up_percent * 100 * shut_speed
+                    if base_down - points > min_down:
+                        cur_base_down = base_down - points
+                    else:
+                        cur_base_down = min_down
+                # 更新价格
+                cur_max_price = cur_price
+
+            # 当前价格低于最高价, 但是大于下跌的反转价格, 认为仍处于上升中
+            elif cur_price >= cur_max_price * (1 - cur_base_down):
+                cur_up_days += 1
+                # cur_min_price可能变了
+                cur_up_percent = cur_max_price / cur_min_price - 1
+                # 更新base_down, 只更新天的就行
+                if shut_by == 0:
+                    if cur_base_down >= min_down + shut_speed:
+                        cur_base_down -= shut_speed
+
+            # 进入下跌
+            else:
+                # 重置cur_min_price
+                cur_min_price = cur_price
+                cur_state = 1
+
+                res_dates.append((1, date_str))
+
+        # 处于下降, 只要判定什么时候恢复上涨就行了
+        elif cur_state == 1:
+            # 虽然在下降, 但是也有可能高于当前最高价格
+            if cur_price > cur_max_price:
+                cur_max_price = cur_price
+
+            # 低于最低价, 依然在下降, 更新最低价
+            if cur_price <= cur_min_price:
+                # 如果跌了上次上涨的down_up_percent, 恢复上涨模式
+                if 1 - cur_price / cur_max_price > cur_up_percent / (1 + cur_up_percent) * down_up_percent:
+                    cur_state = 0
+                    res_dates.append((0, date_str))
+                    # 如果已经有了一定的下得, 则充值max_price
+                    cur_max_price = cur_price
+                cur_min_price = cur_price
+            # 高于最低价, 并突破max_up, 恢复上升
+            elif (cur_price - cur_min_price) / cur_min_price >= max_up:
+                cur_state = 0
+                res_dates.append((0, date_str))
+
+    return res_dates
 
 
 if __name__ == '__main__':
     fix_frame, s01 = resolve_dataframe()
-    find_peak_period(s01)
+    period_dates = find_peak_period(s01)
+    state = 0
+    value0 = 0
+    value1 = 0
+    win_count = 0
+    lose_count = 0
+    for date_group in period_dates:
+        if date_group[0] == 0:
+            value0 = s01[date_group[1]]
+        else:
+            value1 = s01[date_group[1]]
+            if value1 > value0:
+                win_count += 1
+                print "win : " + str(value1 - value0)
+            else:
+                lose_count += 1
+                print "lose : " + str(value1 - value0)
+        print date_group
+        print s01[date_group[1]]
+
+    print "win count : " + str(win_count)
+    print "lose count : " + str(lose_count)
